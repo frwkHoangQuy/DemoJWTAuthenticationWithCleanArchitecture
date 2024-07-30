bf3179bae4af48b8e90edb858675917995b9716a
frwkHoangQuy <frwkHoangQuy@gmail.com>
2024-07-30 16:27:51 +0700

    [FEATURE] Tao pj

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain.Enities;
+namespace Infrastructure.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base( options){ }
+        public DbSet<ApplicationUser> Users { get; set; }
+    }
+}
---
+++ Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection InfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options => 
+                options.UseSqlServer(configuration.GetConnectionString("Default"), 
+                b => b.MigrationsAssembly(typeof(ServiceContainer).Assembly.FullName)), 
+                ServiceLifetime.Scoped); 
+
+            return services;
+        }
+    }
+}
